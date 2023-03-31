833a6cb9b4520195a5940790724f5208a53da646
Jay Dwivedi <jdwivedi@stevens.edu>
2023-03-31 02:57:59 -0400

    sprint 3 commit 3

--- drivers/main_driver.py
+++ drivers/main_driver.py
@@ -1,5 +1,6 @@
 from drivers.sprint_002_driver import sprint_002_main
 from drivers.sprint_003_driver import sprint_003_main
+from drivers.sprint_004_driver import sprint_004_main
 
 if __name__ == '__main__':
     # sprint_001_main(r'./test_results/sprint4_results.txt')
@@ -8,8 +9,8 @@ if __name__ == '__main__':
     #sprint_002_main(r'./test_results/sprint4_results.txt')
     #sprint_002_main()
 
-    sprint_003_main(r'./test_results/sprint4_results.txt')
-    sprint_003_main()
+    # sprint_003_main(r'./test_results/sprint4_results.txt')
+    # sprint_003_main()
 
     # sprint_004_main(r'./test_results/sprint4_results.txt')
-    # sprint_004_main()
+    sprint_004_main()
