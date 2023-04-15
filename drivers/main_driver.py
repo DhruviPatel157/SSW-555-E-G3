28a53968ae25180013ce00f16e1d1f85756a5527
Jay Dwivedi <jdwivedi@stevens.edu>
2023-04-15 05:00:31 -0400

    Sprint 4 - Second Commit

--- drivers/main_driver.py
+++ drivers/main_driver.py
@@ -1,6 +1,7 @@
 from drivers.sprint_001_driver import sprint_001_main
 from drivers.sprint_002_driver import sprint_002_main
 from drivers.sprint_003_driver import sprint_003_main
+from drivers.sprint_004_driver import sprint_004_main
 
 if __name__ == '__main__':
     sprint_001_main(r'./test_results/sprint4_results.txt')
@@ -12,5 +13,5 @@ if __name__ == '__main__':
     #sprint_003_main(r'./test_results/sprint4_results.txt')
     #sprint_003_main()
 
-    # sprint_004_main(r'./test_results/sprint4_results.txt')
-    # sprint_004_main()
+    sprint_004_main(r'./test_results/sprint4_results.txt')
+    sprint_004_main()
